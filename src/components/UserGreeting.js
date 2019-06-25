32e874cf2564b03933a9e29706cac6f50dc2b8dd
Kurs Reacta <kursreacta@MacBook-Pro-3.local>
2019-06-25 14:42:33 +0200

    Add user greeting component

---
+++ src/components/UserGreeting.js
@@ -0,0 +1,21 @@
+import React from "react";
+import jwt from "jsonwebtoken";
+import AuthenticationContext from "../contexts/AuthenticationContext";
+
+function UserGreeting(props) {
+    return (
+        <AuthenticationContext.Consumer>
+            {
+                ({ accessToken }) => <>Witaj {getUserEmail(accessToken)}</>
+            }
+        </AuthenticationContext.Consumer>
+    );
+}
+
+export default UserGreeting;
+
+
+function getUserEmail(accessToken) {
+    const decodedToken = jwt.decode(accessToken);
+    return decodedToken.email;
+}
