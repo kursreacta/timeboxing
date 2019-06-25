9611c99f7dce3975098fb32de0f23a4399b66c68
Kurs Reacta <kursreacta@MacBook-Pro-3.local>
2019-06-25 17:23:46 +0200

    Add inspirational quote to the authenticated app

--- src/components/AuthenticatedApp.js
+++ src/components/AuthenticatedApp.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import EditableTimebox from "./EditableTimebox";
 import TimeboxList from "./TimeboxList";
+import InspirationalQuote from "./InspirationalQuote";
 
 function AuthenticatedApp({ onLogout}) {
     return (
@@ -9,6 +10,7 @@ function AuthenticatedApp({ onLogout}) {
             <Header onLogout={onLogout} />
             <TimeboxList />
             <EditableTimebox />
+            <InspirationalQuote />
         </>
     );
 }
