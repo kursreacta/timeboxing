f07e43085fdbe1281e04611a2c6a389c7f2dfedf
Kurs Reacta <kursreacta@macbook-pro-3.home>
2019-10-24 18:43:49 +0200

    Add AllTimeboxesList

--- src/components/TimeboxesList.js
+++ src/components/TimeboxesList.js
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getAllTimeboxes } from "../reducers";
 
 export function TimeboxesList({ timeboxes, renderTimebox }) {
     return <div className="TimeboxesList">{timeboxes.map(renderTimebox)}</div>;
 }
+const mapStateToProps = (state) => ({ timeboxes: getAllTimeboxes(state)});
+export const AllTimeboxesList = connect(mapStateToProps)(TimeboxesList);
