1f1cb9d9483eff4a49dbe34a78001993fc479c4d
Kurs Reacta <kursreacta@macbook-pro-3.home>
2019-11-03 23:43:20 +0100

    Updates

---
+++ precache-manifest.d7307be388568dfdf8a1d38cc7ec740a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a9ba532cc487d1c8327bf97d6b010e5",
+    "url": "/timeboxing/index.html"
+  },
+  {
+    "revision": "86aa65121381495a9507",
+    "url": "/timeboxing/static/css/main.48c9f70a.chunk.css"
+  },
+  {
+    "revision": "2ececb65d960991b1f6c",
+    "url": "/timeboxing/static/js/2.8e08ea4e.chunk.js"
+  },
+  {
+    "revision": "51ceaf2b2245a840673c",
+    "url": "/timeboxing/static/js/3.fa2db836.chunk.js"
+  },
+  {
+    "revision": "3d32aa603a4cd33d1a34",
+    "url": "/timeboxing/static/js/4.f9c1476b.chunk.js"
+  },
+  {
+    "revision": "c92a9a79963bd2771fe2",
+    "url": "/timeboxing/static/js/5.c3908889.chunk.js"
+  },
+  {
+    "revision": "86aa65121381495a9507",
+    "url": "/timeboxing/static/js/main.d2cf6ba4.chunk.js"
+  },
+  {
+    "revision": "4bd6a58d9e569b0c5e08",
+    "url": "/timeboxing/static/js/runtime~main.112e9714.js"
+  }
+]);
